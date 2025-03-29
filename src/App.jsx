ab75b53fe937b57697cfa0310ae87009a47c8ac2
popeaura <popeahura@gmail.com>
2025-03-29 16:30:02 +0300

    My modified projects folder

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+  const title = 'Welcome to the new Blog';
+  const likes = 50;
+  const link = "http://www.google.com"
+
+  return (
+  <div className="className">
+    <div className="content">
+      <h1>{title}</h1>
+      <p>Liked {likes} times</p>
+
+      <p>{10}</p>
+      <p>{"Hello Ninjas"}</p>
+      <p>{[1,2,3,4,5,6,7]}</p>
+      <p>{Math.random() * 10}</p>
+
+    <a href={link}>Google site</a>
+    </div>
+  </div>
+  );
+}
+
+export default App
