fcd7312ff474472697a6a5d72118afc692bf75b9
popeaura <popeahura@gmail.com>
2025-03-29 22:32:41 +0300

    My modified projects folder

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,7 @@ function App() {
   const link = "http://www.google.com"
 
   return (
-  <div className="className">
-    <div className="content">
-      <h1>{title}</h1>
-      <p>Liked {likes} times</p>
 
-      <p>{10}</p>
-      <p>{"Hello Ninjas"}</p>
-      <p>{[1,2,3,4,5,6,7]}</p>
-      <p>{Math.random() * 10}</p>
-
-    <a href={link}>Google site</a>
-    </div>
-  </div>
   );
 }
 
