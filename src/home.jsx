ab8c8127381ee7d0bb59f25538fc7170da3dd415
popeaura <popeahura@gmail.com>
2025-03-30 12:35:40 +0300

    My modified projects folder

--- src/home.jsx
+++ src/home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     return ( 
         <div className="home">
             <h2>Home Page</h2>
-            <button>Click Me </button>
+            <button onClick={handleClick}>Click Me </button>
         </div>
      );
 } 
