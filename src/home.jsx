36527cf4e0d124b30155e08b077f4b336c4a89d0
popeaura <popeahura@gmail.com>
2025-03-30 14:01:34 +0300

    My modified projects folder

--- src/home.jsx
+++ src/home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <div className="home">
             <h2>Home Page</h2>
             <button onClick={handleClick}>Click Me </button><br /><br />
-            <button onClick={handleClickAgain}>Click Me Again</button>
+            <button onClick={handleClickAgain("Marioo")}>Click Me Again</button>
         </div>
      );
 } 
