0f01009cf4f8bdb0b165010fd9de168b64ff15b3
popeaura <popeahura@gmail.com>
2025-03-31 12:51:48 +0300

    My modified projects folder

--- src/home.jsx
+++ src/home.jsx
@@ -2,22 +2,9 @@ import { useState } from "react";
 
 
 const Home = () => {
-
- const [name, setName]  =  useState('Marioo');
-
- const [age, setAge] = useState(19);
-
- const handleClick = () => {
-    setName('Luigi ');
-    setAge (21);
-} 
-
-
     return ( 
         <div className="home">
             <h2>Home Page</h2>
-            <p>{name} is {age} years  old</p>
-            <button onClick={handleClick}>Click Me</button><br /><br />
         </div>
      );
 } 
