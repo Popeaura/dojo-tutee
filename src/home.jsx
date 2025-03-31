6b993932d2e12cac4b91f09c5dad1eff86b7fcc4
popeaura <popeahura@gmail.com>
2025-03-31 12:08:25 +0300

    My modified projects folder

--- src/home.jsx
+++ src/home.jsx
@@ -1,12 +1,8 @@
 const Home = () => {
 
  const handlClick = () => {
-    console.log("Hello Ninjas");
- }  
- 
- const handleClickAgain = (name) => {
-    console.log("Hello "  + (name));
- }
+
+} 
     return ( 
         <div className="home">
             <h2>Home Page</h2>
