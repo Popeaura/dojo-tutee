c7f583e3a8cdd7ad147b758a9dc0a02a710cef33
popeaura <popeahura@gmail.com>
2025-03-31 13:07:09 +0300

    My modified projects folder

--- src/home.jsx
+++ src/home.jsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 
 const Home = () => {
  const [blogs, setBlogst] = useState ([
-    {title: 'My new website', body: 'lorem ipsum...', author: 'Mario', id: 1}
-    {title: 'Welcome party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2}
+    {title: 'My new website', body: 'lorem ipsum...', author: 'Mario', id: 1},
+    {title: 'Welcome party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2},
     {title: 'Web dev tips', body: 'lorem ipsum...', author: 'Mario', id: 3}
 ])
     return ( 
