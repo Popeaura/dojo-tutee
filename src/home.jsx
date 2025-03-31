0fb88a4a3a3fc86d3602847a87e40906dbdf653a
popeaura <popeahura@gmail.com>
2025-03-31 12:56:23 +0300

    My modified projects folder

--- src/home.jsx
+++ src/home.jsx
@@ -4,7 +4,9 @@ import { useState } from "react";
 const Home = () => {
  const [blogs, setBlogst] = useState ([
     {title: 'My new website', body: 'lorem ipsum...', author: 'Mario', id: 1}
- ])
+    {title: 'Welcome party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2}
+    {title: 'Web dev tips', body: 'lorem ipsum...', author: 'Mario', id: 3}
+])
     return ( 
         <div className="home">
         </div>
