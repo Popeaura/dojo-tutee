4a85c5a4c7aabafaaa03ed1d5aa19eca2493589e
popeaura <popeahura@gmail.com>
2025-03-31 12:01:36 +0300

    My modified projects folder

--- src/home.jsx
+++ src/home.jsx
@@ -2,10 +2,16 @@ const Home = () => {
 
  const handlClick = () => {
     console.log("Hello Ninjas");
- }   
+ }  
+ 
+ const handleClickAgain = (name) => {
+    console.log("Hello "  + (name));
+ }
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handlClick}>Click Me</button><br /><br />
+            <button onClick={() => {handleClickAgain("Ahura")}}>Ninjas </button>
         </div>
      );
 } 
