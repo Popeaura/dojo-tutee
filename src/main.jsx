72effdfe5851d6960299c784d371fd3ac32e7709
popeaura <popeahura@gmail.com>
2025-05-07 16:36:41 +0300

    I uploaded my modifed project's folder

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<<<<<<< HEAD
-=======
 import Home from './home.jsx'
 >>>>>>> 0c82a32c3472238ae9f41cfb61d453166bc04955
 
