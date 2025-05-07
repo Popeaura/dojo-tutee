70595e1323aed0802b568e564f7e670bde01daab
popeaura <popeahura@gmail.com>
2025-05-07 16:10:13 +0300

    I uploaded my modifed project's folder

--- src/App.jsx
+++ src/App.jsx
@@ -21,17 +21,7 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-=======
+        
 import './App.css';
 import Navbar from './Navbar';
 import Home from './home';
