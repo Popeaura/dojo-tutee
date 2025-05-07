0fb1dfb64ec246c07d6a070bb604629c754f18e1
popeaura <popeahura@gmail.com>
2025-05-07 16:11:18 +0300

    I uploaded my modifed project's folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
 
 import './App.css';
 import Navbar from './Navbar';
@@ -24,7 +17,6 @@ function App() {
   </div>
 </div>
   );
->>>>>>> 0c82a32c3472238ae9f41cfb61d453166bc04955
 }
 
 export default App
