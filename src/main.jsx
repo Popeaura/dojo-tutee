2b387b0a01a0d238685e28f83712697c4babcab6
popeaura <popeahura@gmail.com>
2025-05-07 16:36:58 +0300

    I uploaded my modifed project's folder

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Home from './home.jsx'
->>>>>>> 0c82a32c3472238ae9f41cfb61d453166bc04955
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
