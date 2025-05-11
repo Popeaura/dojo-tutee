620ed7001abd8a35d47ba2022cc2abf4dc30f0c3
popeaura <popeahura@gmail.com>
2025-05-11 23:19:06 +0300

    I uploaded my modifed project's folder

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
       };
     
       const handleClickAgain = (name) =>{
-        console.log('Hallo' + )
+        console.log('Hallo' + name)
       }
     return ( 
         <div className="home">
