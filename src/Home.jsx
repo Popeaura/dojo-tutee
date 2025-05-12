b1cb86915217fc15a2eedae975bc17e78e50e81a
popeaura <popeahura@gmail.com>
2025-05-12 17:13:51 +0300

    I uploaded my modifed project's folder

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 const Home = () => {
 
     // let name = 'Mario';
-     const []  = useState('Mario')
+     const [name, ]  = useState('Mario')
 
     const handleClick = () => {
 
