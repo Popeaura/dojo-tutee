2aadbb89f9e52c5adda3f06b5b73c537c37e2d1e
popeaura <popeahura@gmail.com>
2025-05-12 17:23:36 +0300

    I uploaded my modifed project's folder

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,21 +3,20 @@ import { useState } from "react";
 const Home = () => {
 
      const [name,setName ]  = useState('Mario');
-     const [age , setAge] = useState(17);
+     const [age , setAge] = useState('17');
 
     const handleClick = () => {
       setName('Luigi');
       };
     
      const handlClick = () => {
-      setAge(19)
+      setAge('19');
      } 
 
     return ( 
         <div className="home">
             <h2>HomePage</h2>
-            <p>{name}</p>
-            <p>{age}</p>
+            <p>{name} is {age} years old </p>
             <button onClick={handleClick}>Click Me</button>
         </div>
      );
