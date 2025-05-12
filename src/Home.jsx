7256dfacb500c4d4571be9a14bb9e108c23d20fe
popeaura <popeahura@gmail.com>
2025-05-12 16:45:17 +0300

    I uploaded my modifed project's folder

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,7 +11,6 @@ const Home = () => {
         <div className="home">
             <h2>HomePage</h2>
             <button onClick={handleClick}>Click Me</button>
-            <button onClick = {() => handleClickAgain ('Mario', e)}>Click Me again</button>
         </div>
      );
 }
