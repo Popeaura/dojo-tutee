2a6bed3235fcbe0783f04edf16c788292d313f49
popeaura <popeahura@gmail.com>
2025-05-15 11:40:28 +0300

    I uploaded my modifed project's folder

--- src/Home.jsx
+++ src/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
         <div className="home">
           {blogs.map((blog) => (
             <div className="blog-preview" key={blog.id}>
-              <h2></h2> 
+              <h2>{blog}</h2> 
             </div>
           ))}
         </div>
