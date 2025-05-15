ec656f075675d18be6c7f62d9edfaad38173a6c7
popeaura <popeahura@gmail.com>
2025-05-15 11:26:18 +0300

    I uploaded my modifed project's folder

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,6 @@ const Home = () => {
     return ( 
         <div className="home">
             <h2>HomePage</h2>
-            <button onClick={handleClick}>Click Me</button>
         </div>
      );
 }
