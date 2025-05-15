82fb495d19eba215a0e71b2a02da5275ac108983
popeaura <popeahura@gmail.com>
2025-05-15 12:01:35 +0300

    I uploaded my modifed project's folder

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,12 +1,6 @@
 import { useState } from "react";
 const BlogList = () => {
 
-const  [blogs, setBlogs] = useState([
-    {title: 'My new website', body: 'lorem ipsum...', author: 'Mario', id: 1},
-    {title: 'Welcome Party', body: 'lorem ipsum...', author: 'Gray', id: 2},
-    {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Joy', id: 3},
-
-  ])
    
     return ( 
 
