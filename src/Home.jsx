c21927036ff0dbb1da3a2ebafb449cb64963448c
popeaura <popeahura@gmail.com>
2025-05-15 12:11:26 +0300

    I uploaded my modifed project's folder

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,18 +2,17 @@ import { useState } from "react";
 import BlogList from './BlogList';
 
 const Home = () => {
-  const  [blogs, setBlogs] = useState([
-    {title: 'My new website', body: 'lorem ipsum...', author: 'Mario', id: 1},
-    {title: 'Welcome Party', body: 'lorem ipsum...', author: 'Gray', id: 2},
-    {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Joy', id: 3},
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'Mario', id: 1 },
+    { title: 'Welcome Party', body: 'lorem ipsum...', author: 'Gray', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Joy', id: 3 }
+  ]);
 
-  ])
-
-    return ( 
-     <div className="home">
-      <BlogList blog = {blogs}/>
-     </div>
-     );
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} />
+    </div>
+  );
 }
- 
+
 export default Home;
