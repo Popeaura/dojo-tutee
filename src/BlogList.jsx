6adfd9c7319ab7f415335bec129000a3b891cc1f
popeaura <popeahura@gmail.com>
2025-05-15 11:57:03 +0300

    I uploaded my modifed project's folder

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,5 +1,14 @@
 const BlogList = () => {
-    return (  );
+    return ( 
+           <div className="home">
+          {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+              <h2>{blog.title}</h2> 
+              <p>Written by {blog.author}</p>
+            </div>
+          ))}
+        </div>
+     );
 }
  
 export default ;
