391621ab7663b9d409b160781976a0aacd0c8957
popeaura <popeahura@gmail.com>
2025-05-15 11:29:36 +0300

    I uploaded my modifed project's folder

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 const Home = () => {
 
   const  [blogs, setBlogs] = useState([
-    
+    {title: 'My new website', body: 'lorem ipsum...', author: 'Mario', id: 1},
   ])
    
     return ( 
