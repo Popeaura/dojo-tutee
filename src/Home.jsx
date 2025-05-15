5229f75b28ad69c5b746721b9eef784fa0f3df23
popeaura <popeahura@gmail.com>
2025-05-15 11:35:56 +0300

    I uploaded my modifed project's folder

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,8 +11,8 @@ const Home = () => {
    
     return ( 
         <div className="home">
-          {blogs.map(() = >(
-            
+          {blogs.map((blog) => (
+
           ))}
         </div>
      );
