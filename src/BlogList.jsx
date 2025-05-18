3d86bd0daed4bdd690f92c00aa19ad588d193f46
popeaura <popeahura@gmail.com>
2025-05-18 15:54:30 +0300

    I uploaded my modifed project's folder

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,13 +1,12 @@
 const  BlogList= () => {
     return (
         <div className="blog-list">
-               {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-              <h2>{blog.title}</h2> 
-              <p>Written by {blog.author}</p>
-        </div>
-          ))}    
-
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2> 
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}    
         </div>
       );
 }
