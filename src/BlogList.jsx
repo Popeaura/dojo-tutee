1ca30715e35063e2c4c0257207917e5a348ef80a
popeaura <popeahura@gmail.com>
2025-05-18 16:02:18 +0300

    I uploaded my modifed project's folder

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -3,7 +3,7 @@ const  BlogList= (props) => {
     const title = props.title;
     return (
         <div className="blog-list">
-            <h2>{}</h2>
+            <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2> 
